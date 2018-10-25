712c3fe650496fba2dcb88c490216df39c0453bc
C-Bouthoorn <cbouthoorn19@gmail.com>
2018-10-25 19:52:14 +0200

    Fix escape.js

--- src/vue/src/utils/escape.js
+++ src/vue/src/utils/escape.js
@@ -1,17 +1,16 @@
-export default {
-    /**
-     * Replace all characters which might be used to inject HTML with their corresponding special character.
-     */
-    escapeHtml (rawhtml) {
-        // Based on https://stackoverflow.com/a/4835406
-        const specialCharacterMap = {
-            '&': '&amp;',
-            '<': '&lt;',
-            '>': '&gt;',
-            '"': '&quot;',
-            "'": '&#039;'
-        }
-
-        return rawhtml.replace(/[&<>"']/g, (match) => specialCharacterMap[match])
+/**
+ * Replace all characters which might be used to inject HTML with their corresponding special character.
+ */
+export function escapeHtml (rawhtml) {
+    // Based on https://stackoverflow.com/a/4835406
+    const specialCharacterMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
     }
+
+    return rawhtml.replace(/[&<>"']/g, (match) => specialCharacterMap[match])
 }
+
