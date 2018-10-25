10c009150ed69b3cdc91fb45e2751bb503ee22d2
C-Bouthoorn <cbouthoorn19@gmail.com>
2018-10-25 20:09:03 +0200

    Add edge case where the rawhtml is not a string

--- src/vue/src/utils/escape.js
+++ src/vue/src/utils/escape.js
@@ -1,7 +1,10 @@
 /**
  * Replace all characters which might be used to inject HTML with their corresponding special character.
+ * If the rawhtml is not a string, it won't get changed.
  */
 export function escapeHtml (rawhtml) {
+    if (typeof rawhtml !== 'string') return rawhtml;
+
     // Based on https://stackoverflow.com/a/4835406
     const specialCharacterMap = {
         '&': '&amp;',
